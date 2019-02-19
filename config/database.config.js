562c1fb72314e2cd15dcd8c107cb343b325ebee8
AnastasiyaShautsova <shevtsova.anastasia94@gmail.com>
2019-02-19 17:58:15 +0300

    db connection upgrade

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/ntea',
+    urlDev: 'mongodb://localhost:27017/ntea',
+    url: 'mongodb+srv://nbeverages:SILhNj6WDBHqN7D8@cluster0-m2azw.mongodb.net/nbeverages?retryWrites=true',
 };
